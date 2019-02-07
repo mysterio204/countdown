5a0cc59b855aeb30a5260f58b7c505b4883376a5
Thiemo Kersting <thiemo.kersting@continental-corporation.com>
2019-02-07 14:50:19 +0100

    styles

--- countdown.js
+++ countdown.js
@@ -9,26 +9,25 @@ const second = 1000,
 
 
 
-var now = new Date().getTime();
 var end = new Date('Mar 09, 2019 00:14:00').getTime();
 var start = new Date('Jan 28, 2019 00:19:00').getTime();
 
-var timeToGo = end - now;
-var timePassed = now - start;
-var duration = end - start;
-
-var progress= (timePassed/duration)*100;
-
-console.log(duration/(day) );
-console.log(timePassed/(day));
-console.log(progress);
-
-document.getElementById('progress').style.width= progress+"%";
 
 
-    var x = setInterval(function() {
+    setInterval(function() {
+
+        var now = new Date().getTime();
+        var timeToGo = end - now;
+        var timePassed = now - start;
+        var duration = end - start;
+
+        var progress= Math.floor((timePassed/duration)*100);
 
 
+
+
+        document.getElementById('progress').style.width= progress+"%";
+        document.getElementById('progressLabel').innerText = progress+"%";
 
             document.getElementById('weeks').innerText = Math.floor(timeToGo / (week)),
             document.getElementById('days').innerText = Math.floor(timeToGo % (week) / (day)),
@@ -36,33 +35,25 @@ document.getElementById('progress').style.width= progress+"%";
             document.getElementById('minutes').innerText = Math.floor((timeToGo % (hour)) / (minute)),
             document.getElementById('seconds').innerText = Math.floor((timeToGo % (minute)) / second);
 
-        //do something later when date is reached
-        //if (distance < 0) {
-        //  clearInterval(x);
-        //  'IT'S MY BIRTHDAY!;
-        //}
-
-    }, second)
 
 
 
-
-    var y = setInterval(function() {
-
-
-
-
-            document.getElementById('weeks2').innerText = Math.floor(timePassed / (week)),
+        document.getElementById('weeks2').innerText = Math.floor(timePassed / (week)),
             document.getElementById('days2').innerText = Math.floor(timePassed % (week) / (day)),
             document.getElementById('hours2').innerText = Math.floor((timePassed % (day)) / (hour)),
             document.getElementById('minutes2').innerText = Math.floor((timePassed % (hour)) / (minute)),
             document.getElementById('seconds2').innerText = Math.floor((timePassed % (minute)) / second);
 
+
         //do something later when date is reached
         //if (distance < 0) {
         //  clearInterval(x);
         //  'IT'S MY BIRTHDAY!;
         //}
+        console.log("what")
+
+    }, second);
 
 
-    }, second)
+
+
